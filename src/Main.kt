720f4f426e20aa41bb30b92fbe06edf019461438
Admin <reteamboss@gmail.com>
2024-09-15 13:16:43 +0300

    3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Hello World!")
+    var array: ArrayList<Player> = ArrayList<Player>()
+    array.add(Player("Виктор", 100, 20000))
+    array.add(Player("Сергей", 122, 20344))
+    array.add(Player("Джон", 145, 43000))
+    array.add(Player("Том", 123, 24300))
+    array.add(Player("Александр", 133, 43200))
+
+    var totalMoney = array.sumOf { it.money }
+    var totalScore = array.sumOf { it.score }
+
+    println("Общее количество денег $totalMoney")
+    println("Общее количество очков $totalScore")
 }
+
+data class Player(val name: String, val score: Int, val money: Int)
